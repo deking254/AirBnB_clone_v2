a6466c247db798d0899b57c2279c1d02725c5f07
Haile-Baranto <hailebaranto@gmail.com>
2023-08-23 12:19:08 +0300

    Add mysql development envt

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY '13684146@Weldehiet';
+
+-- Grant privileges to the user for the specified databases
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
