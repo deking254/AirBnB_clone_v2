3ba8165e60b571a737517c4e209440938e219a9a
deking254 <dekingsky@gmail.com>
2023-09-22 14:06:44 +0000

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
 """this program is aimed to..."""
+from flask.app import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """this is the function that prints hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """this is the function that prints hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """this is the function that prints hbnb"""
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
