bc0eece0d4fae22ad5ef0ceaf6413328f4ccd406
deking254 <dekingsky@gmail.com>
2023-09-22 11:37:45 +0000

    1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 """this program is aimed to..."""
+from flask.app import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """this is the function that prints hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """this is the function that prints hbnb"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
