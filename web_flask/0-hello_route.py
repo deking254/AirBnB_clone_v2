1f7608491fc3f47f03fe47d4e401a38e84774278
deking254 <dekingsky@gmail.com>
2023-09-22 11:31:29 +0000

    pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """this program is aimed to..."""
-from flask.app import Flask;
-app = Flask(__name__);
+from flask.app import Flask
+app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def index():
     """this is the function that prints hello"""
-    return "Hello HBNB!";
+    return "Hello HBNB!"
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000");
+    app.run(host="0.0.0.0", port="5000")
