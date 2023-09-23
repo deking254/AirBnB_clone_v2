653847e0bfc49b8aff8e869d9a33eb875716b549
deking254 <dekingsky@gmail.com>
2023-09-23 19:33:32 +0000

    7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states():
     """this is the function that prints hello"""
-    print(storage.all())
-    states = [{name:"were", id: 34}]
+    states = storage.all()
     return render_template("7-states_list.html", states=states)
 
 
