e33ac5b63bfda1025edbfc7b9222b55cdd732d44
deking254 <dekingsky@gmail.com>
2023-09-23 20:05:57 +0000

    8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""this program is aimed to..."""
+from flask.app import Flask
+from flask import render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def states():
+    """this is the function that prints hello"""
+    states = storage.all()
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
