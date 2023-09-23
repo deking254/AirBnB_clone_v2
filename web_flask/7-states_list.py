ea9101ce0f7721e9f43b1d603f611947ab884f46
deking254 <dekingsky@gmail.com>
2023-09-23 12:06:08 +0000

    8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""this program is aimed to..."""
+from flask.app import Flask
+from flask import render_template
+from .models import storage
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """this is the function that prints hello"""
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
