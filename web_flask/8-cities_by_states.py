e9de4294f4a328ae0a883059ba1028290152e56c
deking254 <dekingsky@gmail.com>
2023-09-24 08:50:25 +0000

    8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
-def states():
+def cities_bystate():
     """this is the function that prints hello"""
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
